a85831536af3d92f4922895a801133ee487b1d0d
kamlocicho <orwat.kamil3@gmail.com>
2025-06-11 20:53:03 +0200

    full crud endpoitns

--- api/api.py
+++ api/api.py
@@ -15,3 +15,24 @@ def get_posts(request):
 def create_post(request, data: CreateBlogPostSchema):
     post = BlogPost.objects.create(title=data.title, content=data.content)
     return post.toSchema()
+
+@router.delete("/posts/{id}", auth=TokenAuth())
+def delete_post(request, id: int):
+    try:
+        post = BlogPost.objects.get(id=id)
+        post.delete()
+        return {"success": True, "message": "Post deleted."}
+    except BlogPost.DoesNotExist:
+        return {"success": False, "message": "Post not found."}
+
+
+@router.put("/posts/{id}", response=BlogPostSchema, auth=TokenAuth())
+def update_post(request, id: int, data: CreateBlogPostSchema):
+    try:
+        post = BlogPost.objects.get(id=id)
+        post.title = data.title
+        post.content = data.content
+        post.save()
+        return post.toSchema()
+    except BlogPost.DoesNotExist:
+        return {"success": False, "message": "Post not found."}
